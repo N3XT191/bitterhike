ee6bfab2ae500d96ce9b64edfb40aea3239cde53
Marc Bitterli <bimarc@student.ethz.ch>
2023-07-12 14:13:12 +0200

    refactored Wanderungen page

--- pages/wanderungen.tsx
+++ pages/wanderungen.tsx
@@ -12,6 +12,60 @@ const LongHikeSummary = dynamic(() => import("../components/LongHikeSummary"), {
 	ssr: false,
 });
 
+const sections = [
+	{
+		id: "hrp",
+		title: "HRP - Haute Randonnée Pyrénéenne (Aktiv)",
+		description: Array(4).fill("."),
+		sectionGPXUrl: "/HRP/gpx/HRP_done.gpx",
+		fullGPXUrl: "/HRP/gpx/HRP_ganz.gpx",
+		tag: "HRP",
+		focus: true,
+		global: true,
+	},
+	{
+		id: "jura",
+		title: "Jura (Aktiv)",
+		description: [
+			"Vom Kanton Zürich bis nach Nyon, dem ganzen Jura entlang. Das ist der Plan.",
+			"Im Sommer 2021 war ich schon auf dem Lägern. Die weitere Route führt über den Hauenstein, Weissenstein, Chasseral und den Chasseron, so oft wie möglich auf der Krete bis zum Lac de Joux und von da runter nach Nyon",
+			"Unten aufgelistet sind alle Abschnitte die dazu gehören.",
+		],
+		sectionGPXUrl: "/jura/gpx/jura-completed.gpx",
+		fullGPXUrl: "/jura/gpx/jura.gpx",
+		tag: "Jura",
+		focus: true,
+		global: false,
+	},
+	{
+		id: "nswest",
+		title: "Schweiz Nord-Süd Querung 2.0 (Aktiv)",
+		description: [
+			"Seit Beginn der Planungsphase meiner Nord-Süd Querung habe ich mit einer alternativen Route geliebäugelt die im Tessin der Via Alta Verzasca folgt und dann auf 'meiner' Seite des Zürichsees vorbeikommt.",
+			"In 5 Tagesetappen habe ich in den letzten 2-3 Jahren schon einen Teil der Route schon gewandert, der Grossteil steht aber noch aus.  Die Route ist mit 410 km etwas länger als die erste Querung, und hat mit über 29'000 Hm deutlich mehr Höhe!",
+			"Unten aufgelistet sind alle Abschnitte die dazu gehören.",
+		],
+		sectionGPXUrl: "/NSWest/gpx/nswest-completed.gpx",
+		fullGPXUrl: "/NSWest/gpx/nswest.gpx",
+		tag: "Nord-Süd-2.0",
+		focus: true,
+		global: false,
+	},
+	{
+		id: "ns",
+		title: "Schweiz Nord-Süd Querung (Abgeschlossen)",
+		description: [
+			"In 2019, 2020 und 2021 durchquerte ich die Schweiz vom südlichsten Punkt im Tessin zum nördlichsten Punkt im Kanton Schaffhausen in insgesamt 16 Tagesettapen in 5 Abschnitten. Grösstenteils alleine und mit Zelt, im Norden mit kleinem Rucksack in Tagesettappen. Die Route ist insgesamt fast 400km lang und hat 24'500 Hm. Das gibt pro Tag im Schnitt rund 24 km und über 1500 Hm!",
+			"Im Juni mehrfach vom letzten Schnee noch aufgehalten, im August und September aber auch teilweise vom ersten Schnee im Herbst überrascht... Von Alpinen Gratwanderungen bis gemütlichen Waldspaziergang hat diese Route für alle etwas.",
+		],
+		sectionGPXUrl: "/NS/gpx/ns.gpx",
+		fullGPXUrl: "",
+		tag: "Nord-Süd",
+		focus: false,
+		global: false,
+	},
+];
+
 export const getStaticProps = async () => {
 	const files = fs.readdirSync(path.join("posts"));
 	const posts = files.map((filename) => {
@@ -34,7 +88,8 @@ export const getStaticProps = async () => {
 		},
 	};
 };
-const Bio = ({ posts }: { posts: any[] }) => {
+
+const Bio = ({ posts }) => {
 	const MapWidget = React.useMemo(
 		() =>
 			dynamic(() => import("../components/MapWidget"), {
@@ -44,12 +99,55 @@ const Bio = ({ posts }: { posts: any[] }) => {
 		[]
 	);
 
-	const sonstigeWanderungenPosts = posts.filter(
-		(p) =>
-			p.frontMatter.tags?.findIndex((t) => t === "Sonstige Wanderungen") !== -1
-	);
+	const getFilteredPosts = (tag) => {
+		return posts.filter((p) => p.frontMatter.tags?.includes(tag));
+	};
 
 	const [fullScreen, setFullScreen] = useState("");
+
+	const renderLongHikeSummary = (
+		id,
+		title,
+		description,
+		sectionGPXUrl,
+		fullGPXUrl,
+		posts,
+		focus,
+		global
+	) => {
+		return (
+			<LongHikeSummary
+				id={id}
+				title={title}
+				description={description}
+				sectionGPXUrl={sectionGPXUrl}
+				fullGPXUrl={fullGPXUrl}
+				posts={posts}
+				focus={focus}
+				global={global}
+				onToggleFullScreen={(fullScreen) => setFullScreen(fullScreen ? id : "")}
+			/>
+		);
+	};
+
+	const renderLink = (slug, title) => (
+		<li>
+			<Link href={"/blog/" + slug}>
+				<div
+					style={{
+						marginBottom: 10,
+						color: "#0d6efd",
+						fontSize: 20,
+						cursor: "pointer",
+						textDecoration: "underline",
+					}}
+				>
+					{title}
+				</div>
+			</Link>
+		</li>
+	);
+
 	return (
 		<div className="mt-3">
 			<NextSeo
@@ -85,101 +183,31 @@ const Bio = ({ posts }: { posts: any[] }) => {
 						}}
 					>
 						<ul style={{ paddingLeft: useIsMobile() ? 20 : undefined }}>
-							<li>
-								<a href="#hrp">HRP - Haute Randonnée Pyrénéenne (Aktiv)</a>
-							</li>
-							<li>
-								<a href="#jura">Jura (Aktiv)</a>
-							</li>
-							<li>
-								<a href="#nswest">Schweiz Nord-Süd Querung 2.0 (Aktiv)</a>
-							</li>
-							<li>
-								<a href="#ns">Schweiz Nord-Süd Querung (Abgeschlossen)</a>
-							</li>
+							{sections.map((section) => (
+								<li>
+									<a href={"#" + section.id}>{section.title}</a>
+								</li>
+							))}
 							<li>
 								<a href="#daytrips">Sonstige Wanderungen</a>
 							</li>
 						</ul>
 					</Card.Body>
 				</Card>
-				{fullScreen === "" || fullScreen === "hrp" ? (
-					<LongHikeSummary
-						id="hrp"
-						title="HRP - Haute Randonnée Pyrénéenne (Aktiv)"
-						description={Array(4).fill(".")}
-						sectionGPXUrl="/HRP/gpx/HRP_done.gpx"
-						fullGPXUrl="/HRP/gpx/HRP_ganz.gpx"
-						posts={posts.filter(
-							(p) => p.frontMatter.tags.findIndex((t) => t === "HRP") !== -1
-						)}
-						focus={true}
-						global={true}
-						onToggleFullScreen={(fullScreen: boolean) =>
-							setFullScreen(fullScreen ? "hrp" : "")
-						}
-					/>
-				) : undefined}
-				{fullScreen === "" || fullScreen === "jura" ? (
-					<LongHikeSummary
-						id="jura"
-						title="Jura (Aktiv)"
-						description={[
-							"Vom Kanton Zürich bis nach Nyon, dem ganzen Jura entlang. Das ist der Plan.",
-							"Im Sommer 2021 war ich schon auf dem Lägern. Die weitere Route führt über den Hauenstein, Weissenstein, Chasseral und den Chasseron, so oft wie möglich auf der Krete bis zum Lac de Joux und von da runter nach Nyon",
-							"Unten aufgelistet sind alle Abschnitte die dazu gehören.",
-						]}
-						sectionGPXUrl="/jura/gpx/jura-completed.gpx"
-						fullGPXUrl="/jura/gpx/jura.gpx"
-						posts={posts.filter(
-							(p) => p.frontMatter.tags.findIndex((t) => t === "Jura") !== -1
-						)}
-						focus={true}
-						onToggleFullScreen={(fullScreen: boolean) =>
-							setFullScreen(fullScreen ? "jura" : "")
-						}
-					/>
-				) : undefined}
-				{fullScreen === "" || fullScreen === "nswest" ? (
-					<LongHikeSummary
-						id="nswest"
-						title="Schweiz Nord-Süd Querung 2.0 (Aktiv)"
-						description={[
-							"Seit Beginn der Planungsphase meiner Nord-Süd Querung habe ich mit einer alternativen Route geliebäugelt die im Tessin der Via Alta Verzasca folgt und dann auf 'meiner' Seite des Zürichsees vorbeikommt.",
-							"In 5 Tagesetappen habe ich in den letzten 2-3 Jahren schon einen Teil der Route schon gewandert, der Grossteil steht aber noch aus.  Die Route ist mit 410 km etwas länger als die erste Querung, und hat mit über 29'000 Hm deutlich mehr Höhe!",
-							"Unten aufgelistet sind alle Abschnitte die dazu gehören.",
-						]}
-						sectionGPXUrl="/NSWest/gpx/nswest-completed.gpx"
-						fullGPXUrl="/NSWest/gpx/nswest.gpx"
-						posts={posts.filter(
-							(p) =>
-								p.frontMatter.tags.findIndex((t) => t === "Nord-Süd-2.0") !== -1
-						)}
-						focus={true}
-						onToggleFullScreen={(fullScreen: boolean) =>
-							setFullScreen(fullScreen ? "nswest" : "")
-						}
-					/>
-				) : undefined}
-				{fullScreen === "" || fullScreen === "ns" ? (
-					<LongHikeSummary
-						id="ns"
-						title="Schweiz Nord-Süd Querung (Abgeschlossen)"
-						description={[
-							"In 2019, 2020 und 2021 durchquerte ich die Schweiz vom südlichsten Punkt im Tessin zum nördlichsten Punkt im Kanton Schaffhausen in insgesamt 16 Tagesettapen in 5 Abschnitten. Grösstenteils alleine und mit Zelt, im Norden mit kleinem Rucksack in Tagesettappen. Die Route ist insgesamt fast 400km lang und hat 24'500 Hm. Das gibt pro Tag im Schnitt rund 24 km und über 1500 Hm!",
-							"Im Juni mehrfach vom letzten Schnee noch aufgehalten, im August und September aber auch teilweise vom ersten Schnee im Herbst überrascht... Von Alpinen Gratwanderungen bis gemütlichen Waldspaziergang hat diese Route für alle etwas.",
-						]}
-						sectionGPXUrl="/NS/gpx/ns.gpx"
-						fullGPXUrl=""
-						posts={posts.filter(
-							(p) =>
-								p.frontMatter.tags.findIndex((t) => t === "Nord-Süd") !== -1
-						)}
-						onToggleFullScreen={(fullScreen: boolean) =>
-							setFullScreen(fullScreen ? "ns" : "")
-						}
-					/>
-				) : undefined}
+				{sections.map((section) =>
+					fullScreen === "" || fullScreen === section.id
+						? renderLongHikeSummary(
+								section.id,
+								section.title,
+								section.description,
+								section.sectionGPXUrl,
+								section.fullGPXUrl,
+								getFilteredPosts(section.tag),
+								section.focus,
+								section.global
+						  )
+						: null
+				)}
 
 				<div style={{ position: "relative" }}>
 					<h3 id="daytrips">Sonstige Wanderungen</h3>
@@ -190,7 +218,7 @@ const Bio = ({ posts }: { posts: any[] }) => {
 									height={295}
 									sectionLabel="Gewandert"
 									fullLabel="Ganze Route"
-									routeListUrls={sonstigeWanderungenPosts.map(
+									routeListUrls={getFilteredPosts("Sonstige Wanderungen").map(
 										(post) => post.frontMatter.sectionGPXUrl
 									)}
 									focusOn="all"
@@ -201,27 +229,11 @@ const Bio = ({ posts }: { posts: any[] }) => {
 							) : undefined}
 						</div>
 						<ul>
-							{sonstigeWanderungenPosts
+							{getFilteredPosts("Sonstige Wanderungen")
 								.sort((a, b) =>
 									a.frontMatter.date < b.frontMatter.date ? 1 : -1
 								)
-								.map((p) => (
-									<li>
-										<Link href={"/blog/" + p.slug}>
-											<div
-												style={{
-													marginBottom: 10,
-													color: "#0d6efd",
-													fontSize: 20,
-													cursor: "pointer",
-													textDecoration: "underline",
-												}}
-											>
-												{p.frontMatter.title}
-											</div>
-										</Link>
-									</li>
-								))}
+								.map((p) => renderLink(p.slug, p.frontMatter.title))}
 						</ul>
 					</div>
 				</div>
