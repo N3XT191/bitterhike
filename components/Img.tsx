166b5c1c9f1065a7e3c37058a54418c439150604
Marc Bitterli <bimarc@student.ethz.ch>
2023-07-17 19:41:43 +0200

    fullscreen images

--- components/Img.tsx
+++ components/Img.tsx
@@ -19,28 +19,65 @@ const Img = ({ d, src }: { d: string; src: string }) => {
 	}
 	const [width] = useWindowSize();
 
+	const [fullScreen, setFullScreen] = useState(false);
+
 	return (
-		<div
-			ref={ref}
-			style={{
-				marginTop: 30,
-				marginBottom: 30,
-				width: "100%",
-				display: "flex",
-				flexDirection: "column",
-				justifyContent: "center",
-				alignItems: "center",
-				objectPosition: "center",
-			}}
-		>
-			<img
-				width={Math.min(876, width)}
-				style={{ objectFit: "contain", maxHeight: 600 }}
-				src={src}
-				alt="image"
-			/>
-			<div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-				<div style={{ maxWidth: "90%", color: "#888", fontSize: 16 }}>{d}</div>
+		<div>
+			{fullScreen ? (
+				<div
+					style={{
+						position: "fixed",
+						top: 0,
+						left: 0,
+						width: "100%",
+						height: "100vh",
+						backgroundColor: "rgba(0,0,0,0.9)",
+						zIndex: 9999999,
+						display: "flex",
+						justifyItems: "center",
+						alignItems: "center",
+					}}
+					onClick={() => setFullScreen(false)}
+				>
+					<img
+						style={{
+							maxWidth: "100%",
+							maxHeight: "100%",
+							objectFit: "contain",
+							margin: "auto",
+						}}
+						src={src}
+						alt="image"
+					/>
+				</div>
+			) : undefined}
+			<div
+				ref={ref}
+				style={{
+					marginTop: 30,
+					marginBottom: 30,
+					width: "100%",
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "center",
+					alignItems: "center",
+					objectPosition: "center",
+				}}
+				onClick={() => setFullScreen(true)}
+			>
+				<img
+					width={Math.min(876, width)}
+					style={{ objectFit: "contain", maxHeight: 600 }}
+					src={src}
+					alt="image"
+				/>
+				<div
+					style={{ width: "100%", display: "flex", justifyContent: "center" }}
+				>
+					<div style={{ maxWidth: "90%", color: "#888", fontSize: 16 }}>
+						{d}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
